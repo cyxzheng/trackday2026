829a2bcd32857571b50333e247c2497c5605a168
Calvin Zheng <cyxzheng@gmail.com>
2026-05-29 08:34:39 -0700

    small updates + memory

--- announcements/remembering-vic/memory-data.js
+++ announcements/remembering-vic/memory-data.js
@@ -17,14 +17,19 @@
       ],
     },
     {
-      id: "placeholder",
-      author: "YVR GRC Community",
+      id: "calvin",
+      author: "Calvin",
       preview:
-        "A shared memory will live here soon. For now, this placeholder keeps the carousel ready for more voices.",
+        "The last time we saw each other and talked, it was about becoming a parent. You told me to try and get as much sleep as I can.",
       paragraphs: [
-        "A shared memory will live here soon.",
-        "This placeholder is here so the memorial page can show how multiple memories will rotate once more stories are added by the community.",
-        "Replace this entry in memory-data.js with the next submitted memory when it is ready.",
+        "Hi Vic,",
+        "The last time we saw each other and talked, it was about becoming a parent. You told me to try and get as much sleep as I can.",
+        "I'm kicking myself for not going out more to these random meetups and drives, but I told myself it's only for now while I try to figure out my career and family. The last time we talked, you told me it wouldn't actually be that long until I slowly get some me-time.",
+        "I have no foot problems (yet), but from time-to-time you'd still offer me encouragement, or advice, or just be giving me random props for stuff that I make for the club. It was never said, but I really appreciate the support you gave me as I try to figure it out.",
+        "The total time we did spend together was definitely always an event. You were so rapid at our first (and second!) track day together, but somehow we were all slower than the Toyota Echos. It was so fun and awesome to see your competitive side come out, a reminder that despite being the nicest person to everybody, you're definitely no pushover.",
+        "I don't know why you agreed to randomly show up at my house before our very first \"large\" parking lot meet, but I'm glad our friendship started off with me offering to wash your wheels on your (then) brand-new Circuit. It's hard to forget that. It'll be hard to forget the last time we saw each other too.",
+        "Rest easy, my friend. Gonna miss you.",
+        "- Calvin",
       ],
     },
   ];
